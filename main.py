b6542c679d0d2cb09bd4c4724940966da9238a4e
Jeri <ladyjp_08@yahoo.com>
2018-10-30 17:53:55 -0500

    css-blogpage-db

--- main.py
+++ main.py
@@ -11,49 +11,76 @@ app.secret_key = 'randomish8key'
 class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120))
+    title = db.Column(db.String(200))
     body = db.Column(db.String(10000))
     
 
     def __init__(self, title, body):
         self.title = title
         self.body = body
-        
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    return render_template('blog.html', title="A Black Woman's Blog")
 
+       
+@app.route('/blog')
+def blog():
+    blogtitle = request.form['blogtitle']
+    body = request.form['body']
+   
+    return render_template('blog.html', blogtitle=blogtitle, body=body)
+
+@app.route('/newpost',methods=['POST', 'GET'])
+def newpost():
     if request.method == 'POST':
-        blog_name = request.form['blog']
-        new_blog = Blog(blog_name)
+        blogtitle = request.form['blogtitle']
+        body = request.form['body']
+        new_blog = Blog(title_post, blog_post)
         db.session.add(new_blog)
         db.session.commit()
 
-    blogs = Blog.query.filter_by(completed=False).all()
-    completed_blogs = Blog.query.filter_by(completed=True).all()
-    return render_template('blog.html',title="Black Woman Blog!", 
-        blogs=blogs, completed_blogs=completed_blogs)
+    else:
+        return render_template('newpost.hmtl')
+
+    
+#@app.route('/', methods=['POST'])
+#def blog_page():
+   
+
+
+
+if __name__ == '__main__':
+    app.run()
+
+#@app.route('/', methods=['POST'])
+#def view_post():
+   # title_post = request.form['title']
+    #blog_post = request.form['blog']
+    #return title_post
+
+    #blogs = Blog.query.filter_by(completed=False).all()
+    #completed_blogs = Blog.query.filter_by(completed=True).all()
+    #return render_template('blog.html',title="Black Woman Blog!", 
+       # blogs=blogs, completed_blogs=completed_blogs)
 
 #@app.route('/blog', methods=['POST'])
 #def blog():
 
 
-@app.route('/newpost')
-def newpost():
-    return render_template('newpost.html')
 
 
 
-@app.route('/delete-post', methods=['POST'])
-def delete_post():
 
-    task_id = int(request.form['task-id'])
-    task = Task.query.get(task_id)
-    task.completed = True
-    db.session.add(task)
-    db.session.commit()
+#@app.route('/delete-post', methods=['POST'])
+#def delete_post():
 
-    return redirect('/')
+    #task_id = int(request.form['task-id'])
+    #task = Task.query.get(task_id)
+    #task.completed = True
+    #db.session.add(task)
+    #db.session.commit()
+
+   # return redirect('/')
 
 
-if __name__ == '__main__':
-    app.run()
